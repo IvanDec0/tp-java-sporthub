9cb89736d45aa5897286d4dd72c19406d57c81a6
sebafermanelli <sebastianfermanelli@gmail.com>
2025-11-17 11:04:24 -0300

    Merge

--- src/main/java/com/java/sportshub/services/CloudflareR2Service.java
+++ src/main/java/com/java/sportshub/services/CloudflareR2Service.java
@@ -7,7 +7,6 @@ import java.net.URL;
 import java.time.Duration;
 import java.util.Locale;
 import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
